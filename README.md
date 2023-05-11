2643c36c3e7908b7375f4afa276d04be5747f2d6
martin-lin-cw <martin6061097@gmail.com>
2023-05-11 18:15:10 +0800

    create model and store interface

--- README.md
+++ README.md
@@ -2,10 +2,36 @@
 
 ## Setup project
 
-1. init go project with
+1. Init go project with
 
    `go mod init github.com/...`
 
-2. install goose with
+2. Install goose with
 
    `go install github.com/pressly/goose/v3/cmd/goose@latest`
+
+## Create Reddit model
+
+1. Define structure for
+
+   - Thread, Post, and Comment
+
+2. Create interface for manage those model
+   - ThreadStore
+     - Thread: get a thread by id
+     - Threads: get all threads
+     - CreateThread
+     - UpdateThread
+     - DeleteThread
+   - PostStore
+     - Post: get a post by id
+     - PostsByThread: get all post belong to a thread id
+     - CreatePost
+     - UpdatePost
+     - DeletePost
+   - CommentStore:
+     - Comment: get a comment by id
+     - CommentsByPost: get all comments belong to a post id
+     - CreateComment
+     - UpdateComment
+     - DeleteComment
---
+++ goreddit.go
@@ -0,0 +1,46 @@
+package goreddit
+
+type Thread struct {
+	Id          int64  `db:"id"`
+	Title       string `db:"title"`
+	Description string `db:"description"`
+}
+
+type Post struct {
+	Id       int64  `db:"id"`
+	ThreadId int64  `db:"thread_id"`
+	Title    string `db:"title"`
+	Content  string `db:"content"`
+	Votes    int    `db:"votes"`
+}
+
+type Comment struct {
+	Id      int64  `db:"id"`
+	PostId  int64  `db:"post_id"`
+	Content string `db:"content"`
+	Votes   int    `db:"votes"`
+}
+
+type ThreadStore interface {
+	Thread(id int64) (Thread, error)
+	Threads() ([]Thread, error)
+	CreateThread(t *Thread) error
+	UpdateThread(t *Thread) error
+	DeleteThread(id int64) error
+}
+
+type PostStore interface {
+	Post(id int64) (Post, error)
+	PostsByThread(threadId int64) ([]Post, error)
+	CreatePost(p *Post) error
+	UpdatePost(p *Post) error
+	DeletePost(id int64) error
+}
+
+type CommentStore interface {
+	Comment(id int64) (Comment, error)
+	CommentsByPost(postId int64) ([]Comment, error)
+	CreateComment(c *Comment) error
+	UpdateComment(c *Comment) error
+	DeleteComment(id int64) error
+}
